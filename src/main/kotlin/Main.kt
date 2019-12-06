063d5f18f94fb27df413f26e612e8393d9f3f0af
Jungbin Kim <mail@jungbin.kim>
2019-12-06 17:35:20 +0900

    First commit with KotlinRx
    - pull vs push, iterator vs observer

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,22 @@
+import io.reactivex.Observable
+import io.reactivex.rxkotlin.subscribeBy
+import io.reactivex.rxkotlin.toObservable
+
+fun main(args: Array<String>) {
+    var list: List<Any> = listOf("One", 1, 2, "Three")
+    // 리스트의 데이터를 pull 해서 가져오는 동작 방식
+//    var iterator = list.iterator()
+//    while(iterator.hasNext()) {
+//        println(iterator.next())
+//    }
+
+    var observable: Observable<Any> = list.toObservable()
+
+    // Observable 구독
+    observable.subscribeBy(
+        onNext = { println(it) }, // 변경사항이 onNext로 Push
+        onError = { it.printStackTrace() },
+        onComplete = { println("DONE!") }
+    )
+
+}
